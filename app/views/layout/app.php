1b2106e3baf02e485c7a83a8821272a6c2b3fde7
eivydas <eivydas@users.noreply.github.com>
2018-01-04 16:42:31 +0200

    Update to Bootstrap v4.0.0-beta.3

--- app/views/layout/app.php
+++ app/views/layout/app.php
@@ -6,8 +6,8 @@
 
     <title>Cardinity</title>
 
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css"
+          integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?php echo getenv('PUBLIC_ROOT'); ?>/css/app.css">
 </head>
 <body>
